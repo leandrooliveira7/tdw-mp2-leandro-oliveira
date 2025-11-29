269ac2ae2e6411bededef4e58627cec121a9821d
Leandro Oliveira <leandrokexxoliv@gmail.com>
2025-11-29 17:34:00 +0000

    style: adjust layout and image width in Tracklist and MovieDetail components for improved presentation

--- src/components/Movies/movieDetail.jsx
+++ src/components/Movies/movieDetail.jsx
@@ -28,7 +28,7 @@ const MovieDetail = () => {
           <img
             src={posterSrc}
             alt={movie.title}
-            className="w-85 rounded-lg object-cover"
+            className="w-90 rounded-lg object-cover"
           />
         </div>
 
