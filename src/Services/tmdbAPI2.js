534db932048201393ca09918765407a0e71aaf84
Leandro Oliveira <leandrokexxoliv@gmail.com>
2025-11-29 21:34:00 +0000

    style: rename getMovieByGenre to getMoviesByGenre for consistency and clarity

--- src/Services/tmdbAPI2.js
+++ src/Services/tmdbAPI2.js
@@ -2,13 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tmdbApi2 = createApi({
   reducerPath: "tmdbApi2",
-  baseQuery: fetchBaseQuery({ baseUrl: "/.netlify/functions/" }), // chama a função serverless
+  baseQuery: fetchBaseQuery({ baseUrl: "/.netlify/functions/" }),
   endpoints: (builder) => ({
-    getMovieByGenre: builder.query({
+    getMoviesByGenre: builder.query({
       query: (genreId) =>
         `getMoviesByGenre?genreId=${encodeURIComponent(genreId)}`,
     }),
   }),
 });
 
-export const { useGetMovieByGenreQuery } = tmdbApi2;
+export const { useGetMoviesByGenreQuery } = tmdbApi2;
--- src/Services/tmdbAPI4.js
+++ src/Services/tmdbAPI4.js
@@ -5,8 +5,7 @@ export const tmdbApi4 = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "/.netlify/functions/" }),
   endpoints: (builder) => ({
     getMovieBySearch: builder.query({
-      query: (searchQuery) =>
-        `getMovieBySearch?query=${encodeURIComponent(searchQuery)}`,
+      query: (query) => `getMovieBySearch?query=${encodeURIComponent(query)}`,
     }),
   }),
 });
