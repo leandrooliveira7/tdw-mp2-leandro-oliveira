bb02411a4b9af583bdc9320734130348d9d03fc0
Leandro Oliveira <leandrokexxoliv@gmail.com>
2025-11-29 19:13:38 +0000

    style: replace hardcoded TMDB token with environment variable for security

--- src/Services/tmdbAPI2.js
+++ src/Services/tmdbAPI2.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTQ1MDZmYWI4YzNkOGQwZmZiNTdhNjUyZGU2ZTY2MyIsIm5iZiI6MTcyOTMzNDU2MC42NzUsInN1YiI6IjY3MTM4ZDIwMGNiNjI1MmY5OTA4NDA3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Us1O5vY34bX__8fB6HNzL6gDXEBQHvRXJjqE25shLBY';
+const token = process.env.TMDB_TOKEN;
   
 export const tmdbApi2 = createApi({
   reducerPath: "tmdbApi2",
